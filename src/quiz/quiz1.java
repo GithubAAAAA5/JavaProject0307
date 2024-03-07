bbb6f425c9b88f493ef3776d4819dc631fee77a4
GirHubAAAAA5 <tjoeun@DESKTOP-85LBU83.mshome.net>
2024-03-07 16:33:31 +0900

    Javaproject0307

---
+++ src/quiz/quiz1.java
@@ -0,0 +1,20 @@
+package quiz;
+
+public class quiz1 {
+
+	public static void main(String[] args) {
+		System.out.println("===================");
+		System.out.println();
+		System.out.println("       /)/)        ");
+		System.out.println("      (  ..)       ");
+		System.out.println("      (  >ㅁ        ");
+		System.out.println(" Have a Good Time. ");
+        System.out.println("===================");
+        System.out.println();
+        System.out.println();
+        
+        
+        System.out.println("======================");
+	}
+
+}
