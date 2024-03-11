55d163b92a51962355d97971ad88cc7364fb3445
GirHubAAAAA5 <tjoeun@DESKTOP-85LBU83.mshome.net>
2024-03-11 16:40:35 +0900

    240311

---
+++ src/quiz/ArrayQuiz.java
@@ -0,0 +1,13 @@
+package quiz;
+
+public class ArrayQuiz {
+
+	public static void main(String[] args) {
+		// 1. 1000개짜리 int형 배열을 생성한 뒤 모든 값을 1 - 100사이의 
+		//    램덤 숫자로 채워보세요
+		
+		// 2. 1000개의 램덤 숫자들 중 각 숫자가 몇번씩 나왔는지 세어 출력하세요 
+		
+	}
+
+}
