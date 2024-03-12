90249b9e523127b9f3f11f44966c31c3a170b0fb
GirHubAAAAA5 <tjoeun@DESKTOP-85LBU83.mshome.net>
2024-03-12 18:16:35 +0900

    240312

--- src/quiz/starEx.java
+++ src/quiz/starEx.java
@@ -1,33 +1,38 @@
 
 package quiz;
 
+import java.util.Scanner;
+
 public class starEx {
 
 	public static void main(String[] args) {
-		// 
-		int dan = 5;
+		
+
+	Scanner scan = new Scanner(System.in);
+
+		System.out.println("줄 수를 입력해주세요 : ");
+		int dan = scan.nextInt();
 		
 		for (int i = 0; i < dan; i++) {
-			for (int j = 0; j < dan - i - 1; j++) {
+			for(int j = 0; j < dan - i - 1; j++) {
 				System.out.print(" ");
 			}
-			for (int j = 0; j < i*2 + 1; j++) {
+			for (int j = 0; j < i * 2 + 1; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 		
-		int dan2 = 4;
-		
-		for (int o = 0; o < dan2; o++) {
-			for (int p = 0; p < o + 1; p++) {
+		for (int i = 0; i < dan - 1; i++) {
+			for(int j = 0; j < i + 1; j++) {
 				System.out.print(" ");
-		}
-			for (int p = 0; p < 2*dan2 - (2*o) - 1; p++) {
+			}
+			for(int j = 0; j< 2 * (dan - i - 1) - 1; j++) {
 				System.out.print("*");
-		}
+			}
 			System.out.println();
-	}
+		}
 
 	}
+	
 }
