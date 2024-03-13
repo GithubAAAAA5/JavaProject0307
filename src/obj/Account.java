2b6d30e5ce43f04229ade3232352e67380ad9c59
GirHubAAAAA5 <tjoeun@DESKTOP-85LBU83.mshome.net>
2024-03-13 16:38:28 +0900

    240313

---
+++ src/obj/Account.java
@@ -0,0 +1,17 @@
+package obj;
+
+public class Account {
+
+	public int acc;
+	public int dep;
+	
+	public String name;
+	public String pass;
+	
+	public double interest;
+	
+	public int Deposit() {
+		return acc;
+	}
+
+}
