4d2683abb7ec9828cfbd6651cbefce9662e54be6
GirHubAAAAA5 <tjoeun@host.docker.internal>
2024-03-20 18:48:36 +0900

    240320 p

--- src/a1_practice/p9.java
+++ src/a1_practice/p9.java
@@ -14,9 +14,36 @@ public class p9 {
 //	int multiply(int x, int y) {
 //		return x*y;
 		
+		Operator op = new Operator();
 		
+		System.out.println(op.multiply(4,3));
+		System.out.println(op.multiply(4.5, 3.5));
+		System.out.println(op.multiply(4, 3.5));
+		System.out.println(op.multiply(4.5, 3));
 		
 		
 
+	}
+}
+
+class Operator {
+	int multiply(int x, int y) {
+		System.out.println("(int, int)");
+		return x * y;
+	}
+	
+	double multiply(double x , double y ) {
+		System.out.println("(double, double)");
+		return x * y;
+	}
+	
+	double multiply (int x , double y ) {
+		System.out.println("(int, double)");
+		return x*y;
+	}
+	
+	double multiply (double x , int y ) {
+		System.out.println("(double, int)");
+		return x*y;
 	}
 }
