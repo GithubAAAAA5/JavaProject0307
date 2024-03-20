27e01f8e1eac0e7a3621088cf881ac9278a8dbde
GirHubAAAAA5 <tjoeun@host.docker.internal>
2024-03-20 11:06:53 +0900

    240320 1c

--- src/obj/exception/Child.java
+++ src/obj/exception/Child.java
@@ -5,14 +5,14 @@ import java.sql.SQLException;
 
 public class Child extends Parents {
 
-	@Override
-	public void doIt() throws SQLException {		// 부모가 throws 하지 않은 Exception은 throws 할 수 없다.
-		System.out.println("Child.doIt");			// 부모는 IOException 를 throws 했기때문에
-		try {										// SQLException 은 불가능
-			super.doIt();
-		} catch (IOException e) {
-			throw new SQLException(e.getMessage());
-		}
-	}
+//	@Override
+////	public void doIt() throws SQLException {		// 부모가 throws 하지 않은 Exception은 throws 할 수 없다.
+//		System.out.println("Child.doIt");			// 부모는 IOException 를 throws 했기때문에
+//		try {										// SQLException 은 불가능
+//			super.doIt();
+//		} catch (IOException e) {
+//			throw new SQLException(e.getMessage());
+//		}
+//	}
 
 }
