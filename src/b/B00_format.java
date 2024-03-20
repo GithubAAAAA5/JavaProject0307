f331d91379d0770db2cefed9fa74e732d4a0a020
GirHubAAAAA5 <tjoeun@host.docker.internal>
2024-03-20 11:55:59 +0900

    b move

---
+++ src/b/B00_format.java
@@ -0,0 +1,42 @@
+package b;
+
+public class B00_format {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		System.out.printf("a의 값은 :%d",a);  //제어문자로 %d는 정수이다.
+		/*
+		 * 		정수 : %d
+		 * 		실수 : %f
+		 * 		한문자 : %c
+		 * 		문자열 : %s
+		 * 		각 제어문자와 대응하는 값을 지정해야한다.
+		 */
+		
+		float b = 10.234f;
+		char c = 'A';
+		String str = "오늘 날씨는 미세먼지 많음 이다.";
+		
+		System.out.printf("\na의 값은 정수 %d, b의 값은 실수 %f \nc의 값은 문자 %c, str의 값은 문자열 %s",a,b,c,str);
+		
+		System.out.println();
+		a = 100;
+		System.out.printf("[%10d]",a);		//제어문자 앞에 숫자는 출력 값의 자릿수 ->[       100] 라고 출력되는데 %10d 는 10칸을 의미하고 10칸중 a(100)이 차지하는 3자리 제외 7자리가 공백을고 표시된다.
+		System.out.println();
+		System.out.printf("[%-10d]",a);
+		
+		System.out.println();
+		b = 33.145f;
+//		System.out.printf("[%10f]",b);	--> 출력문자는 [ 33.145000]
+		System.out.printf("[%10.2f]",b);	//실수제어 문자의 자리수 지정은 ".숫자" 를 사용한다. --> 출력문자는 [     33.15] 로 끝자리가 반올림처리 되었다.
+		System.out.println();
+		System.out.printf("[%10.3f]",1000.123457);  // 투입값이 1000.123547 인경우 출력값은 1000.124 가나온다 고로 반올림된것을 알수있다.
+		System.out.println();
+		System.out.printf("[%7.3f]",1000.123655);
+		System.out.println();
+		System.out.printf("[%010d]",a);
+		
+	}
+
+}
---
+++ src/b/B08_variableLifeTime.java
@@ -0,0 +1,68 @@
+package b;
+
+public class B08_variableLifeTime {
+	
+	static int c;	// 클래스 멤버변수					- 클래스에 직접 사용하는 변수로 객체 생성과는 상관이 없다.
+	int b = 8;		// 인스턴스 멤버 변수 ( 필드, 속성 )	- 객체가 생성되어야 쓸 수 있다.
+	
+
+	public static void main(String[] args) {
+		// 변수의 수명
+		
+		// {} 블럭 내부에서 선언된 변수는 '지역 변수' 이다. {}을 벗어나면 사라진다.
+		// 변수가 살아 있는 범위를 변수의 영역(scope)이라고 부른다.
+		// 변수의 영역만 겹치지 않는다면 같은 이름의 변수를 사용할 수 있다.
+		
+		int b = 5;	// 지역변수
+		System.out.println("mian method의 b : "+ b);
+		c = 10;
+		System.out.println("Class 멤버변수 c :"+ c);
+		
+		
+		// 객체 생성
+		B08_variableLifeTime obj = new B08_variableLifeTime();	//객체 생성
+		System.out.println("class의 b(인스턴스 멤버변수) : "+obj.b);
+		System.out.println();
+		B08_variableLifeTime obj2 = new B08_variableLifeTime();	//객체 생성
+		obj2.b = 100;	// obj2 객체의 b인스턴스 변수에 값을 수정
+		System.out.println("class의 b(obj 인스턴스 멤버변수) : "+obj.b);
+		System.out.println("class의 b(obj2 인스턴스 멤버변수) : "+obj2.b);
+		
+		System.out.println();
+		
+		// 클래스 변수
+		System.out.println(c);
+		System.out.println("obj.c에 값을 대입하기 전 : " +obj.c);
+		obj.c = 20;
+		System.out.println("obj.c에 값을 대입한 후 : " +obj.c);
+		System.out.println(obj.c); 
+		System.out.println("obj2.c에 값을 대입한 후 : " +obj2.c);
+		System.out.println(obj2.c); 
+		B08_variableLifeTime.c = 200;
+		System.out.println("대입한 후 obj.c: " +obj.c);
+		System.out.println("대입한 후 obj2.c : " +obj2.c);
+		
+		if(true) {   // 무조건 실행하겠다. 항상 실행하겠다.
+			int a = 10; //지역변수 설정
+			System.out.println(a);
+			a = 7;
+			System.out.println("main 메서드의 b : "+b);   // 선언하지않아도 이미 어미문에서 선언을 했기때문에 {}안에서 다시 지정하지 않아도 b의 값이 나온다.
+///			int b = 10;  								// {}안에 있어도 이미 어미문에서 선언을 했기때문에 중복이 될수없다.
+		}
+///		a = 7;  // if문에서 a가 지정이 되었지만 {} 밖으로 나온순간 a의 값을 지정해주어야 하기때문에 빨간줄로 성립할수없다고 경고창이 나온다.
+		
+		if(true) {
+			int a = 20;
+			System.out.println(a);  					// 위 아래 if문은 서로 별개의 {} 안의 내용이기 때문에 같은 int a 의 이름을 가져도 다른 값을 가지는게 가능하다.
+			
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			//for 문에서 사용되는 int i (초기값) {} 내에서 선언된 지역 변수 이다.
+			//int i 의 값은 for문을 벗어 날 수 없다. 
+		}
+		
+		
+	}
+
+}
