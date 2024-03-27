f9b64503497d719e05dc2f2549c952704830881c
GirHubAAAAA5 <tjoeun@host.docker.internal>
2024-03-27 13:15:50 +0900

    240327 c4

--- src/H03_ThreadEx2.java
+++ src/H03_ThreadEx2.java
@@ -1,5 +1,7 @@
 
 public class H03_ThreadEx2 {
+	
+	
 	/*
 	 *  [스레드 상태 제어]**
 	 *   스레드 작업시 프로그램 중지 및 버그가 발생할 수 있는 부분 
@@ -12,7 +14,11 @@ public class H03_ThreadEx2 {
 	 *    
 	 */
 
+	
+	
 	public static void main(String[] args) {
+		
+		
 		
 		//sleep()
 		Thread t1 = new Thread(new Runnable() {
@@ -42,6 +48,8 @@ public class H03_ThreadEx2 {
 			}
 		});
 		
+		
+		
 		t1.start();
 		
 		// interrupt() 메서드			//interrupt 는 try catch 를 방지한다.. 이게무슨말일까
@@ -57,25 +65,57 @@ public class H03_ThreadEx2 {
 		
 		t1.interrupt();	// 스레드가 일시 정지 상태이면 예외 발생. sleep interrupted 출력
 
-		// join() 메서드
-		Sum t2 = new Sum();
-		Sum t3 = new Sum();
 		
-		t2.start();
-		t3.start();
+		
+//		// join() 메서드
+//		Sum t2 = new Sum();
+//		Sum t3 = new Sum();
+//		
+//		t2.start();
+//		t3.start();
+//		
+//		try {
+//			t2.join();	// t2 스레드가 종료될때까지 대기한다.
+//			t3.join();	// t3 스레드가 종료될떄까지 대기한다.
+//		} catch (InterruptedException e) {
+//			System.out.println(e.getMessage());
+//		}
+//		System.out.println(" 두 스레드의 Sum의 합계 : " + (t2.sum + t3.sum));
+		
+		
+		
+		// yield() : 다른 스레드에게 실행을 양보한다.
+		YieldThread t4 = new YieldThread();
+		YieldThread t5 = new YieldThread();
+		
+		t4.start();
+		t5.start();
 		
 		try {
-			t2.join();	// t2 스레드가 종료될때까지 대기한다.
-			t3.join();	// t3 스레드가 종료될떄까지 대기한다.
-		} catch (InterruptedException e) {
-			System.out.println(e.getMessage());
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {			
 		}
-		System.out.println(" 두 스레드의 Sum의 합계 : " + (t2.sum + t3.sum));
+		t4.isContinue = false;		// t4 스레드 yield 가 동작한다. 즉, 양보한다.
+	
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		t4.isContinue = true;		// t4 가 true 로 다시 실행된다.
 		
-		// yeild() 
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		
+		// 스레드 종료
+		t4.isBreak = true;
+		t5.isBreak = true;
 	}
 
 }
+
+
 
 class Sum extends Thread {
 	int sum = 0;
@@ -84,5 +124,25 @@ class Sum extends Thread {
 		for (int i =1; i <= 100 ; i++) {
 			sum += i;
 		}
+	}
+}
+
+
+
+class YieldThread extends Thread {
+	boolean isBreak = false;
+	boolean isContinue = true;
+	
+	@Override
+	public void run() {
+		while (!isBreak) {
+			if(isContinue) {
+				System.out.println(getName() + " 실행 중");
+			}else {
+				System.out.println("yield 실행");
+				Thread.yield();			// 자원 양보 .-> 효율적인 자원 사용을 위해서 사용한다.
+			}
+		}
+		System.out.println(getName() + " 종료");
 	}
 }
