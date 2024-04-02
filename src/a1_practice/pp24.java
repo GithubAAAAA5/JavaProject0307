e2778d27794ea4c00ea1b5ec2a36d6989591e69f
GirHubAAAAA5 <tjoeun@host.docker.internal>
2024-04-02 09:54:57 +0900

    240402 c1

--- src/a1_practice/pp24.java
+++ src/a1_practice/pp24.java
@@ -20,7 +20,7 @@ public class pp24 {
 		for (int i=0; i<=2; i++) {
 			for (int j=0; j<=2; j++) {
 				for (int k =0; k<=2; k++) {
-					System.out.println(arrInt[i][j][k] + "\t");
+					System.out.print(arrInt[i][j][k] + "\t");
 				}
 				System.out.println();
 			}
